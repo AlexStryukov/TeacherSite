109a8c2e232ce3a1b99dffe4f21fcce2248d57a5
Alexander Stryukov <strjukov00@gmail.com>
2022-04-28 13:04:15 +0300

    Change bugs logo

--- teachersite/headapp/urls.py
+++ teachersite/headapp/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
     path('method_work/', method_work, name='method_work'),
-    path('achievements', achievements, name='achievements'),
-    path('feedback', feedback, name='feedback'),
-    path('post/<int:post_id>/', show_post, name='posts')
+    path('achievements/', achievements, name='achievements'),
+    path('feedback/', feedback, name='feedback'),
+    path('post/<int:post_id>/', show_post, name='posts'),
+    path('category/<int:cat_id>/', show_category, name='category'),
 ]
