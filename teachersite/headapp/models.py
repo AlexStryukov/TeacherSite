734c09739ccb08cdde142bed7538f19f2e2400c0
AlexStr <strjukov00@gmail.com>
2022-04-28 22:36:34 +0300

    change admins panel

--- teachersite/headapp/models.py
+++ teachersite/headapp/models.py
@@ -3,13 +3,13 @@ from django.urls import reverse
 
 
 class News(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_upload = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Содержимое статьи')
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d", verbose_name='Фотография')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_upload = models.DateTimeField(auto_now=True, verbose_name='Изменено')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
@@ -17,10 +17,20 @@ class News(models.Model):
     def get_absolute_url(self):
         return reverse('posts', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Новости'
+        verbose_name_plural = 'Новости'
+        ordering = ['time_create', 'title']
+
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
 
     def __str__(self):
         return self.name
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Категории'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
