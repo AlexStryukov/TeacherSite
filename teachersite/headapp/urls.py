8083bad8a4d7ff860a378538d722ce32418c0814
Alexander Stryukov <strjukov00@gmail.com>
2022-05-11 14:05:04 +0300

    some resylts

--- teachersite/headapp/urls.py
+++ teachersite/headapp/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('method_work/', method_work, name='method_work'),
     path('achievements/', achievements, name='achievements'),
     path('feedback/', feedback, name='feedback'),
-    path('post/<int:post_id>/', show_post, name='posts'),
-    path('category/<int:cat_id>/', show_category, name='category'),
+    path('post/<slug:post_slug>/', show_post, name='posts'),
+    path('category/<slug:cat_slug>/', show_category, name='category'),
 ]
